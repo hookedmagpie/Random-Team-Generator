31e43411bed4664c7565943eef440444254e03b3
mewtnewt <cbaker24601@gmail.com>
2016-06-28 19:34:02 -0400

    added connection.php

---
+++ inc/connection.php
@@ -0,0 +1,15 @@
+<?php
+try {
+    $db = new PDO(
+        'mysql:host=localhost;dbname=pokemon',
+        'root',
+        '');
+    $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+} catch (Exception $e) {
+    echo "Unable to Connect";
+    echo $e->getMessage();    
+    exit;
+}
+
+
+var_dump($db);
